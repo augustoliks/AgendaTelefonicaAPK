c11b3000e435af3ca09adffdc09fff918c63104a
Carlos <carlos.santos110@fatec.sp.gov.br>
2018-10-06 16:46:00 -0300

    OK: Antes de copiar as arquivos

--- app/src/main/java/com/br/agenda/FormularioActivity.java
+++ app/src/main/java/com/br/agenda/FormularioActivity.java
@@ -1,7 +1,9 @@
 package com.br.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,15 +17,20 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        Button botao = (Button)findViewById(R.id.formulario_botao_salvar);
+        Button botao = (Button) findViewById(R.id.formulario_botao_salvar);
         botao.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Toast.makeText(FormularioActivity.this, "Botao Clicado", Toast.LENGTH_LONG).show();
+                        finish();
                     }
                 }
-
         );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/br/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/br/agenda/ListaAlunosActivity.java
@@ -1,9 +1,16 @@
 package com.br.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -13,7 +20,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_lista_alunos);
 
-        String[] alunos ={"Daniel","Ronaldo","Jeferson","Felipe"};
+        String[] alunos = {"Daniel", "Ronaldo", "Jeferson", "Felipe"};
 
         //Recebe uma instancia de um atributo view e converte em ListView
         ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
@@ -23,6 +30,16 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         //Adiciona na instancia do .XML o conteudo do adaptado
         listaAlunos.setAdapter(adapter);
+
+
+        Button botao = (Button) findViewById(R.id.lista_alunos_botao);
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
